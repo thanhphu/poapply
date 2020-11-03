2ca3ed68c714cce43f4f0cdc4ab58667ffe91ea1
Thanh Phu <git@thanhphu.net>
2020-11-03 11:51:29 +0900

    Fix bugs, add debug configuration

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 'use strict';
 
 const pofile = require('pofile');
-const yargs = require('yargs');
 const replaceInFiles = require('replace-in-files');
-
-const argv = yargs
-  .command('apply', 'Apply translated po files to source code', {
-    pofile: {
-      description: 'translated po file',
-      alias: 'f',
-      type: 'string'
-    },
-    files: {
-      description: 'source code to apply transaltion',
-      alias: 'o',
-      type: 'string'
-    }
+const yargs = require("yargs")
+  
+let { argv } = yargs
+  .scriptName('poapply')
+  .usage("Usage: $0 -f po-file -o source-files")
+  .option('f', {
+    alias: 'translated',
+    type: 'string',
+    nargs: 1
   })
-  .help()
-  .alias('help', '?')
-  .argv;
+  .option('o', {
+    alias: 'sources',
+    type: 'string',
+    nargs: 1
+  })
+  .normalize('f')
+  .normalize('o');
 
-if (argv._.includes('apply')) {
-  pofile.load(argv.pofile, (err, po) => {
+let { translated, sources } = argv
+translated = translated.trim();
+sources = sources.trim();
+
+if (translated && sources) {
+  pofile.load(translated, (err, po) => {
     if (err) {
       console.log(err);
       exit(1);
     }
     po.items.forEach((item) => {
       replaceInFiles({
-        files: argv.files,
+        files: sources,
         from: item.msgid,
         to: item.msgstr[0]
       })
