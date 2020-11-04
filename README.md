89bbd851618b1b0d889f1b71d53f42ae200e7f8e
Thanh Phu <git@thanhphu.net>
2020-11-04 15:56:31 +0900

    Add target file reading, arguments

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # poapply
-Apply po translations to source code
+A simple utility to apply po translations to source code
+
+## What it does:
+1. Iterate through all entries in po files (-f)
+2. Replace the first occurence of each entry in target file (-o)
+
+## How to use
+`poapply -f pofile -o target-file`
+
--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 const pofile = require('pofile');
 const replaceInFiles = require('replace-in-files');
-const yargs = require("yargs")
-  
+const yargs = require("yargs");
+const fs = require('fs');
+
 let { argv } = yargs
   .scriptName('poapply')
-  .usage("Usage: $0 -f po-file -o source-files")
+  .usage("Usage: $0 -f po-file -o source-file")
   .option('f', {
     alias: 'translated',
     type: 'string',
@@ -24,18 +25,22 @@ let { translated, sources } = argv
 translated = translated.trim();
 sources = sources.trim();
 
+function checkErr(err) {
+  if (err) {
+    console.log(err);
+    exit(1);
+  }
+}
+
 if (translated && sources) {
-  pofile.load(translated, (err, po) => {
-    if (err) {
-      console.log(err);
-      exit(1);
-    }
-    po.items.forEach((item) => {
-      replaceInFiles({
-        files: sources,
-        from: item.msgid,
-        to: item.msgstr[0]
-      })
-    })
-  })
+  fs.readFile(sources, 'utf8', (err, fileContent) => {
+    checkErr(err);
+    pofile.load(translated, (err, po) => {
+      checkErr(err);
+      po.items.forEach((item) => {
+        fileContent = fileContent.replace(item.msgid, item.msgstr[0]);
+      });
+      fs.writeFile(sources + '.out', fileContent, checkErr);
+    });
+  });
 }
